4c42a37c7fa3e671adb127398f5f73eb0590e5e8
Nikerty <nikertylitik@gmail.com>
2023-11-20 21:22:25 +0300

    Route that gives answer

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify, json
 from .main import assistant
+from answer_creating import assistant
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['POST'])
 def assistant_route():
+    rq = json.loads(request.get_data(as_text=True))['question']
+    rs = assistant(rq)
+    return jsonify(response=rs)
+
+@app.route("/answer", methods=['GET'])
+def main_route():
     rq = json.loads(request.get_data(as_text=True))['question']
     rs = assistant(rq)
     return jsonify(response=rs)
